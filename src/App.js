cf6db62bfd6a941fda75f59f7133ad5ec41a7909
jaw <62795911+JacobWestby@users.noreply.github.com>
2022-10-31 10:46:43 +0100

    added room name state that updates when new room name is ev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
+import { useState } from "react";
+
 import "./input.css";
 
+import Background from "./components/Background";
+import Phone from "./components/Phone";
+
+const colors = {
+  purple: "#9381FF",
+  black: "#363946",
+  white: "#FEF6EC",
+  blue: "#A7E2E3"
+};
+
+
 const App = () => {
+  const [showCreate, setShowCreate] = useState(false);
+  const [inputText, setInputText] = useState("Room Name");
+
   return (
     <header className=" h-screen w-screen relative">
       <div className=" h-screen w-screen flex flex-col items-center bg-transparent absolute py-11">
         <div className=" flex flex-col items-center h-[18vh] justify-between">
           <h2 className=" font-semibold text-xl">Welcome to</h2>
-          <h1 className=" font-semibold text-[2rem]">TalkSpace</h1>
+          <h1 className=" font-semibold text-[2rem]" style={{ color: colors.blue }}>TalkSpace</h1>
+        </div>
+
+        <div className="w-screen h-full flex justify-evenly items-center">
+          <Phone createRoom={inputText} />
+          <div className="flex flex-col justify-between items-center h-[40%] sm:w-[20%] z-20 transition-all duration-300 ease-in-out">
+            {showCreate
+              ? null
+              : <button className=" p-5 rounded-2xl shadow-xl font-semibold" style={{ backgroundColor: colors.white }}>Join Room</button>
+            }
+
+            {showCreate
+              ? <form className=" h-[60%] w-[80%] sm:w-full flex flex-col justify-between">
+                <label htmlFor="Name" className="text-center font-semibold text-lg" style={{ color: colors.white }}>Room Name</label>
+                <input type="text" className=" rounded-2xl sm:h-9 focus:outline-none px-2 text-center" value={inputText} onChange={(e) => setInputText(e.target.value)} />
+                <button className="rounded-2xl shadow-xl font-semibold sm:h-9" style={{ backgroundColor: colors.white }}>Create</button>
+              </form>
+              : <button onClick={() => setShowCreate(true)} className=" p-5 rounded-2xl shadow-xl font-semibold" style={{ backgroundColor: colors.white }}>Create Room</button>
+            }
+          </div>
         </div>
       </div>
 
-
-
-
-
-
-      <div className=" absolute z-[-1]">
-        <div className=" w-screen h-[35vh] bg-[#363946]" id="bg_top">{/*  Background top */}</div>
-        <div className=" w-screen h-[65vh] bg-[#9381FF]" id="bg_bot">{/*  Background bottom */}</div>
-      </div>
-
+      <Background />
     </header>
   )
 }
