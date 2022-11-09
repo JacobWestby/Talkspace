f586b25aeb6c6a9d7298dcd437fdd5e3dfe26724
jaw <62795911+JacobWestby@users.noreply.github.com>
2022-11-09 22:41:03 +0100

    added axios to call the backend. Rooms and chat now come from MongoDB

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid';
+import { useState, useEffect } from 'react';
 
 // Components
 import Home from "./pages/Home";
@@ -7,6 +8,7 @@ import JoinRoomPage from './pages/JoinRoomPage';
 import ChatRoomPage from './pages/ChatRoomPage';
 
 import "./input.css";
+import axios from 'axios';
 
 const colors = {
   purple: "#9381FF",
@@ -15,108 +17,108 @@ const colors = {
   blue: "#A7E2E3"
 };
 
-const rooms =
-  [
-    {
-      name: "Room 1",
-      chatting: Math.floor(Math.random() * 6),
-      id: uuidv4(),
-      chat: [
-        {
-          name: "John",
-          message: "Hello world",
-          id: uuidv4(),
-          local: false,
-        },
-        {
-          name: "Sara",
-          message: "Hello John",
-          id: uuidv4(),
-          local: true,
-        },
-        {
-          name: "Sara",
-          message: "Hello John, i am so cool and i have a new hat",
-          id: uuidv4(),
-          local: true,
-        },
-      ]
-    },
-    {
-      name: "Room 2",
-      chatting: Math.floor(Math.random() * 6),
-      id: uuidv4(),
-      chat: [
-        {
-          name: "John",
-          message: "New Room who dis",
-          id: uuidv4(),
-          local: true,
-        },
-        {
-          name: "John",
-          message: "Wow that's soo cool dsadsadadsadsadsadsdsadsadasdsadas",
-          id: uuidv4(),
-          local: false,
-        },
-        {
-          name: "John",
-          message: "Can u feel it?!",
-          id: uuidv4(),
-          local: true,
-        },
+// const rooms =
+//   [
+//     {
+//       name: "Room 1",
+//       chatting: Math.floor(Math.random() * 6),
+//       id: uuidv4(),
+//       chat: [
+//         {
+//           name: "John",
+//           message: "Hello world",
+//           id: uuidv4(),
+//           local: false,
+//         },
+//         {
+//           name: "Sara",
+//           message: "Hello John",
+//           id: uuidv4(),
+//           local: true,
+//         },
+//         {
+//           name: "Sara",
+//           message: "Hello John, i am so cool and i have a new hat",
+//           id: uuidv4(),
+//           local: true,
+//         },
+//       ]
+//     },
+//     {
+//       name: "Room 2",
+//       chatting: Math.floor(Math.random() * 6),
+//       id: uuidv4(),
+//       chat: [
+//         {
+//           name: "John",
+//           message: "New Room who dis",
+//           id: uuidv4(),
+//           local: true,
+//         },
+//         {
+//           name: "John",
+//           message: "Wow that's soo cool dsadsadadsadsadsadsdsadsadasdsadas",
+//           id: uuidv4(),
+//           local: false,
+//         },
+//         {
+//           name: "John",
+//           message: "Can u feel it?!",
+//           id: uuidv4(),
+//           local: true,
+//         },
 
-      ],
-    },
-    {
-      name: "News room",
-      chatting: Math.floor(Math.random() * 6),
-      id: uuidv4(),
-      chat: [],
-    },
-    {
-      name: "Cool guys room",
-      chatting: Math.floor(Math.random() * 6),
-      id: uuidv4(),
-      chat: [],
-    },
-    {
-      name: "Room for chicks",
-      chatting: Math.floor(Math.random() * 6),
-      id: uuidv4(),
-      chat: [],
-    },
-    {
-      name: "Room for dudes",
-      chatting: Math.floor(Math.random() * 6),
-      id: uuidv4(),
-      chat: [],
-    },
-    {
-      name: "Aliens only",
-      chatting: Math.floor(Math.random() * 6),
-      id: uuidv4(),
-      chat: [],
-    },
-    {
-      name: "Are cows real?",
-      chatting: Math.floor(Math.random() * 6),
-      id: uuidv4(),
-      chat: [],
-    },
-    {
-      name: "Hello world",
-      chatting: Math.floor(Math.random() * 6),
-      id: uuidv4(),
-      chat: [],
-    },
-    {
-      name: "Goodmorning",
-      chatting: Math.floor(Math.random() * 6),
-      id: uuidv4(),
-      chat: [],
-    },
-  ];
+//       ],
+//     },
+//     {
+//       name: "News room",
+//       chatting: Math.floor(Math.random() * 6),
+//       id: uuidv4(),
+//       chat: [],
+//     },
+//     {
+//       name: "Cool guys room",
+//       chatting: Math.floor(Math.random() * 6),
+//       id: uuidv4(),
+//       chat: [],
+//     },
+//     {
+//       name: "Room for chicks",
+//       chatting: Math.floor(Math.random() * 6),
+//       id: uuidv4(),
+//       chat: [],
+//     },
+//     {
+//       name: "Room for dudes",
+//       chatting: Math.floor(Math.random() * 6),
+//       id: uuidv4(),
+//       chat: [],
+//     },
+//     {
+//       name: "Aliens only",
+//       chatting: Math.floor(Math.random() * 6),
+//       id: uuidv4(),
+//       chat: [],
+//     },
+//     {
+//       name: "Are cows real?",
+//       chatting: Math.floor(Math.random() * 6),
+//       id: uuidv4(),
+//       chat: [],
+//     },
+//     {
+//       name: "Hello world",
+//       chatting: Math.floor(Math.random() * 6),
+//       id: uuidv4(),
+//       chat: [],
+//     },
+//     {
+//       name: "Goodmorning",
+//       chatting: Math.floor(Math.random() * 6),
+//       id: uuidv4(),
+//       chat: [],
+//     },
+//   ];
 
 // TODO: ADD ROOMS/CHATS TO MONGO DB AND GET THEM FROM THERE
 // TODO: SETUP UP LOGIN OR AUTO COOKIE AND ID FOR USER
@@ -124,6 +126,19 @@ const rooms =
 // TODO: MAKE SURE YOU CAN RELOAD PAGE WITHOUT AN ERROR
 
 const App = () => {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    const getRooms = async () => {
+      const res = await axios.get('/api/rooms');
+      setRooms(res.data);
+    };
+    getRooms();
+  }, []);
+
+  console.log(rooms);
+
+
   return (
     <BrowserRouter>
       <Routes>
